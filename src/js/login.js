3d4250999c485f931fd1381ca2124bc324829699
JOJO <1246374352@qq.com>
2018-08-22 00:29:08 +0800

    登录页面样式布局完成

---
+++ src/js/login.js
@@ -0,0 +1,16 @@
+/* 
+* @Author: Marte
+* @Date:   2018-08-21 23:00:11
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-08-21 23:04:02
+*/
+
+;(function(){
+
+    jQuery(function($){
+
+        $('#j_header').load('../html/header&footer.html .j_header');
+        $('#j_footer').load('../html/header&footer.html .j_footer');
+    })
+
+})();
