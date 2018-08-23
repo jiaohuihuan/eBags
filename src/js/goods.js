37e1bdf58b100a0694b138c32523d9cecf4e2336
JOJO <1246374352@qq.com>
2018-08-23 23:48:09 +0800

    详情页布局完成

---
+++ src/js/goods.js
@@ -0,0 +1,10 @@
+//引入header&footer
+;(function(){
+
+    jQuery(function($){
+
+        $('#j_header').load('../html/header&footer.html .j_header');
+        $('#j_footer').load('../html/header&footer.html .j_footer');
+    })
+
+})();
